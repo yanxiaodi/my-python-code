6f5abc4f995769605f7ffd203459daa6b5408143
yanxiaodi <yan_xiaodi@hotmail.com>
2024-02-03 16:30:29 +1300

    Delete some code

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
 print("hello world!")
-
-# Write a function that takes a string as input and returns the string reversed.
-def reverseString(s):
-    return s[::-1]
-
-print(reverseString("hello"))
-
