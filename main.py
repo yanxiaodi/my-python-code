e71aab8d2258390dbd8d823d720a798d44a4a20d
yanxiaodi <yan_xiaodi@hotmail.com>
2024-02-03 16:27:39 +1300

    Add a simple function

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello world!")
+
+# Write a function that takes a string as input and returns the string reversed.
+def reverseString(s):
+    return s[::-1]
+
+print(reverseString("hello"))
+
